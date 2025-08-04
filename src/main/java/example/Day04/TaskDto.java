a13a5a85d6f9e27dc387a43390f6cc1308fe326b
JeonghoonAHN0510 <jeonghoonahn0510@gmail.com>
2025-08-04 10:30:13 +0900

    Spring DAY04 @RequestParam + @RequestBody

---
+++ src/main/java/example/Day04/TaskDto.java
@@ -0,0 +1,25 @@
+package example.Day04;
+
+public class TaskDto {
+    // 1. 멤버변수
+    private String name;
+    private int age;
+    // 2. 생성자
+    public TaskDto() {}
+    public TaskDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    // 3. 메소드
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    @Override
+    public String toString() {
+        return "TaskDto{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    } // func end
+} // class end
