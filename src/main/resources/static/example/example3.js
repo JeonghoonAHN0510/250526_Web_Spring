d972ca03922a40c49e870acd5bfed857b7b99092
JeonghoonAHN0510 <jeonghoonahn0510@gmail.com>
2025-08-06 11:26:17 +0900

    Spring DAY06 JSP 설치

--- src/main/resources/static/example/example3.js
+++ src/main/resources/static/example/example3.js
@@ -1,8 +1,9 @@
 console.log('example3.js connect');
 
+// 1) 비동기 fetch : fetch 실행되고 결과가 오기 전에 뒤에 있는 코드를 실행한다.
 const func1 = ( ) => {
     console.log('[1] fetch 전');
-    // 비동기 fetch : fetch 실행되고 결과가 오기 전에 뒤에 있는 코드를 실행한다.
+    // 비동기 fetch
     let option = { method : "GET" };
     fetch( "/day06/exam1", option )
         .then( response => response.json() )
@@ -16,8 +17,20 @@ const func1 = ( ) => {
 } // func end
 // 코드 실행 결과 : [1] -> [3] -> [2] (비동기 fetch이기 때문에)
 
-const func2 = ( ) => {
-    console.log('func2 exe');
+// 2) 동기 fetch : fetch 실행되고 결과가 오는 것을 기다리고 다음 코드를 실행한다.
+const func2 = async ( ) => {
+    console.log('[1] fetch 전');
+    // 동기 fetch
+    const option = { method : "GET" };
+    try{
+    const response = await fetch( "/day06/exam1" , option );
+    const data = await response.json();
+    console.log( data );
+    } catch ( error ){
+        console.log( error );
+    }
+    console.log('[2] fetch 통신 결과')
 
-
+    console.log('[3] fetch 후')
 } // func end
+// 코드 실행 결과 : [1] -> [2] -> [3] (비동기 fetch이기 때문에)
